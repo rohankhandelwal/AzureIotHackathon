35e258612a8caf860340f3c2461cd145ee63a1c8
Sudeep Pradhan <spradhan@microsoft.com>
2019-02-25 13:11:29 -0800

    Samples

---
+++ AzureIoT.Hackathon.Device.Client/TaskHelper.cs
@@ -0,0 +1,30 @@
+﻿namespace AzureIoT.Hackathon.Device.Client
+{
+    using System;
+    using System.Threading.Tasks;
+
+    public static class TaskHelper
+    {
+        public static void IgnoreFault(this Task task)
+        {
+            if (task.IsCompleted)
+            {
+                var ignored = task.Exception;
+            }
+            else
+            {
+                task.ContinueWith(t => { var ignored = t.Exception; }, TaskContinuationOptions.OnlyOnFaulted | TaskContinuationOptions.ExecuteSynchronously);
+            }
+        }
+
+        public static void Fork(this Task thisTask)
+        {
+            if (thisTask == null)
+            {
+                throw new InvalidOperationException("Fork a null task!");
+            }
+
+            thisTask.ContinueWith(t => Console.WriteLine($"Task faulted: {t.Exception}"), TaskContinuationOptions.OnlyOnFaulted);
+        }
+    }
+}
